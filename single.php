08a57932d6f84bdf76452bd098ba653ce0d1a3fb
Jonathan <jhendricker@gmail.com>
2020-02-18 10:03:43 -0500

    Overwrite the original single.php for Posts
    Add's in the featured image / post_thumbnail to the single post page.

---
+++ single.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Basic
+ * Template Post Type: degree
+ */
+?>
+<?php get_header(); the_post(); ?>
+
+<div class="container mb-5 mt-3 mt-lg-5">
+	<article class="<?php echo $post->post_status; ?> post-list-item">
+		<?php 
+		if ( has_post_thumbnail() )		
+			the_post_thumbnail('medium', array( 'class' => 'rounded float-left mr-3' ));
+		
+		the_content();		
+		?>
+	</article>
+</div>
+
+<?php get_footer(); ?>
