4964fd4ce8a200889d6ef95809c85c596f51bc9e
Jonathan <jhendricker@gmail.com>
2020-02-21 11:20:33 -0500

    Added the published date to the single.php file for better layout for single posts.

--- single.php
+++ single.php
@@ -1,19 +1,18 @@
 <?php
 /**
  * Template Name: Basic
- * Template Post Type: degree
  */
 ?>
 <?php get_header(); the_post(); ?>
 
 <div class="container mb-5 mt-3 mt-lg-5">
-	<article class="<?php echo $post->post_status; ?> post-list-item">
+	<article class="<?php echo $post->post_status; ?> post-list-item">		
 		<?php 
 		if ( has_post_thumbnail() )		
-			the_post_thumbnail('medium', array( 'class' => 'rounded float-left mr-3' ));
-		
-		the_content();		
+			the_post_thumbnail('medium', array( 'class' => 'rounded float-left mr-3' )); 
 		?>
+		<div class="meta mb-3"><span class="date"><?php the_time( 'F j, Y' ); ?></span></div>
+		<?php   the_content();	?>
 	</article>
 </div>
 
